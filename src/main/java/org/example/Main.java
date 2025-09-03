f57ec2db6eb8fb31219078245c39c155622ea666
gobird <monaka@example.com>
2025-09-03 21:39:38 +0900

    first-commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class Main {
+
+  /**
+   * 課題 →文字列結合と数値計算結果を結合して、「自分の名前+ "さんの年齢は、" + 自分の年齢 +"歳です。"」とターミナルに出力する。
+   *
+   * @param args
+   */
+  public static void main(String[] args) {
+    String name = "あっきー";
+
+    int age = 15 + 16;
+
+    System.out.println(name + "さんの年齢は、" + age + "歳です。");
+
+  }
+}
